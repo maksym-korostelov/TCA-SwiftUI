e4f71f7f737314d42a3cb2e6a730d9cd4ba65df5
Maksym Korostelov <Maksym_Korostelov@epam.com>
2025-02-27 19:20:42 +0100

    initial commit

---
+++ TCA-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA-SwiftUI
+//
+//  Created by Maksym Korostelov on 27/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA-SwiftUI/TCA_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_SwiftUIApp.swift
+//  TCA-SwiftUI
+//
+//  Created by Maksym Korostelov on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TCA_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
